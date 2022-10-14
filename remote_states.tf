ad3787799d9ce211313bad77262e05f374d12bbd
David Redmin <david.redmin@trio.dhs.gov>
2022-10-14 10:52:16 -0400

    Add necessary remote state

---
+++ remote_states.tf
@@ -0,0 +1,22 @@
+# ------------------------------------------------------------------------------
+# Retrieves state data from a Terraform backend. This allows use of
+# the root-level outputs of one or more Terraform configurations as
+# input data for this configuration.
+# ------------------------------------------------------------------------------
+
+data "terraform_remote_state" "users" {
+  backend = "s3"
+
+  config = {
+    encrypt        = true
+    bucket         = "cisa-cool-terraform-state"
+    dynamodb_table = "terraform-state-lock"
+    profile        = "cool-terraform-backend"
+    region         = "us-east-1"
+    key            = "cool-accounts/users.tfstate"
+  }
+
+  # There is only one environment for this account, so there is
+  # no need to set up "production" and "staging" remote states.
+  workspace = "production"
+}
