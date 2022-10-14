2c3c15f8f4c86484576eef9b5f2b40c4219f1bb6
David Redmin <david.redmin@trio.dhs.gov>
2022-10-14 10:55:33 -0400

    Create local variable for caller username

---
+++ locals.tf
@@ -0,0 +1,14 @@
+# ------------------------------------------------------------------------------
+# Retrieve the effective Account ID, User ID, and ARN in which Terraform is
+# authorized.  This is used to calculate the session names for assumed roles.
+# ------------------------------------------------------------------------------
+data "aws_caller_identity" "default" {}
+
+# ------------------------------------------------------------------------------
+# Evaluate expressions for use throughout this configuration.
+# ------------------------------------------------------------------------------
+locals {
+  # Extract the user name of the current caller for use
+  # as assume role session names.
+  caller_user_name = split("/", data.aws_caller_identity.default.arn)[1]
+}
