abbf8cf73ff54a0f6b01ede818b936b4ae7a3c9e
David Redmin <david.redmin@trio.dhs.gov>
2022-10-14 10:56:24 -0400

    Add admin provisioner policy

---
+++ policies.tf
@@ -0,0 +1,29 @@
+# The policy document that allows assumption of all roles needed in order to
+# provision any environment.
+data "aws_iam_policy_document" "provision" {
+  statement {
+    actions = [
+      "sts:AssumeRole",
+      "sts:TagSession",
+    ]
+
+    effect = "Allow"
+
+    resources = [
+      "arn:aws:iam::*:role/${var.provision_role_name}",
+      "arn:aws:iam::*:role/${var.startstopssmsession_role_name}",
+    ]
+
+    sid = "AllowAllAccountRoles"
+  }
+}
+
+# The policy that allows assumption of all roles needed in order to provision
+# assessment environments.
+resource "aws_iam_policy" "provision" {
+  provider = aws.users
+
+  description = var.provision_policy_description
+  name        = var.provision_policy_name
+  policy      = data.aws_iam_policy_document.provision.json
+}
